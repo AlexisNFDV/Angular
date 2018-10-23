0aee7f067503e99dfd2145e459e04039b77a07f7
Dillies Benoît <benoit.dillies@ynov.com>
2018-10-23 17:28:52 +0200

    Mistake model employee

--- Back/models/Employee.js
+++ Back/models/Employee.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const EmployeeSchema = mongoose.Schema({
     firstname: {type: String},
     lastname: String,
+    username: String,
     day_of_birth: {type: Date, default: Date.now},
     address: String,
     phone_number: Number,
